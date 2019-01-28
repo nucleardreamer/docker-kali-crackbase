9bc29fa18b71769693f4a9bc967d6fe0f166d21c
Flynn Joffray <nucleardreamer@gmail.com>
2019-01-28 15:52:31 -0800

    WIP scripts and tools

--- Dockerfile
+++ Dockerfile
@@ -5,16 +5,29 @@ ARG DEBIAN_FRONTEND="noninteractive"
 WORKDIR /work
 
 RUN apt-get update -y && apt-get install -y \
-  samdump2 \
-  bkhive \
+  curl \
   john \
   sleuthkit \
+  net-tools \
   autopsy \
-  dcfldd
+  dcfldd \
+  multiarch-support
+
+ADD http://http.us.debian.org/debian/pool/main/b/bkhive/bkhive_1.1.1-1_amd64.deb /tmp/bkhive.deb
+ADD http://http.us.debian.org/debian/pool/main/s/samdump2/samdump2_1.1.1-1.1_amd64.deb /tmp/samdump2.deb
+ADD http://security.debian.org/debian-security/pool/updates/main/o/openssl/libssl1.0.0_1.0.1t-1+deb8u10_amd64.deb /tmp/libssl.deb
+
+RUN dpkg -i /tmp/libssl.deb \
+  && dpkg -i /tmp/bkhive.deb \
+  && dpkg -i /tmp/samdump2.deb
 
 COPY entrypoint.sh .
 
 RUN chmod +x entrypoint.sh \
-  && apt-get autoremove -y
+  && apt-get autoremove -y \
+  && apt-get clean \
+  && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 
 ENTRYPOINT [ "/work/entrypoint.sh" ]
+
+EXPOSE 9999
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,15 +2,37 @@
 
 if [[ $# -lt 1 ]]; then
   echo ""
+  echo "  Note: All files get written to /output"
   echo "  Usage:"
   echo ""
-  echo $"    $0 mkimg <device> <name>                 "
+  echo "  > mkimg <device> <filename>                  "
+  echo "    # example: mkimg /dev/sda mybackup.dd "
+  echo ""
+  echo "  > samdump <partition> <hivepath> <filename>              "
+  echo "    # example: samdump /dev/sda1 Windows/System32/config myhashes"
+  echo ""
+  echo "  > shell                                      "
   echo ""
   exit 1
 fi
 
 case $1 in
   mkimg)
-    exec dcfldd if=${2} hash=md5 of=/output/${3} bs=512
+    dcfldd if=${2} hash=md5 of=/output/${3} bs=512
+    ;;
+  samdump)
+    DEVICE=$2
+    MP="/tmp/mount/${2}"
+    mkdir -p /tmp/mount
+    mount -t ntfs ${2} ${MP}
+    sync
+    pushd ${MP}
+    bkhive SYSTEM /output/hive.txt
+    samdump2 SAM /output/hive.txt > /output/hash.txt
+    sync
+    umount ${MP}
+    sync
+  shell)
+    exec /bin/bash
     ;;
 esac
